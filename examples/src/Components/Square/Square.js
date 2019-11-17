35654d2529361af5b5014b9610f2fb83eb0f448b
Halil Koca <ihalilkoca@gmail.com>
2019-11-17 13:42:32 +0300

    calculate winner  and Square function added

--- examples/src/Components/Square/Square.js
+++ examples/src/Components/Square/Square.js
@@ -1,31 +1,40 @@
 import React from "react";
 import "./Square.css";
 
-class Square extends React.Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     value: null
-  //   };
-  // }
-
-  render() {
-    return (
-      //1. <button className="square" onClick={function () { alert("click"); }}></button>
-      //2. <button className="square" onClick={() => { alert("click"); }}>
-      <button
-        className="square"
-        // onClick={() => { this.setState({ value: "X" }); }}
-        onClick={() => this.props.onClick()}
-      >
-        {
-          this.props.value
-          /* {this.state.value} */
-        }
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
+
+
+// class Square extends React.Component {
+//   // constructor(props) {
+//   //   super(props);
+
+//   //   this.state = {
+//   //     value: null
+//   //   };
+//   // }
+
+//   render() {
+//     return (
+//       //1. <button className="square" onClick={function () { alert("click"); }}></button>
+//       //2. <button className="square" onClick={() => { alert("click"); }}>
+//       <button
+//         className="square"
+//         // onClick={() => { this.setState({ value: "X" }); }}
+//         onClick={() => this.props.onClick()}
+//       >
+//         {
+//           this.props.value
+//           /* {this.state.value} */
+//         }
+//       </button>
+//     );
+//   }
+// }
 
 export default Square;
